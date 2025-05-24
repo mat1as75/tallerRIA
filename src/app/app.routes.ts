2963fbb942ed57ab450d275c0183fcac021c49d3
santiago <sgonzalr767@gmail.com>
2025-05-24 15:44:03 -0300

    Iniciar Sesion y Registrarse front

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { ProductCatalogComponent } from './components/product-catalog/product-catalog.component';
 import { ViewProductComponent } from './components/view-product/view-product.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 export const routes: Routes = [
     {path: 'catalogo', component: ProductCatalogComponent},
-    {path: 'producto/:id', component: ViewProductComponent}    
+    {path: 'producto/:id', component: ViewProductComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent}
 ];
