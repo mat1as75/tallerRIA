a2f22ff30a5b95bcd86925cb9866727923dc89b3
Askrahs <gastonfernandez1999@gmail.com>
2025-05-25 21:48:20 -0300

    Alta Producto, Listar Productos y Listar Ordenes de Productos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { ProductCatalogComponent } from './components/product-catalog/product-catalog.component';
-import { ViewProductComponent } from './components/view-product/view-product.component';
+import { ProductCreateComponent } from './components/product-create/product-create.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductOrdersComponent } from './components/product-orders/product-orders.component';
+//import { ViewProductComponent } from './components/view-product/view-product.component';
 
 export const routes: Routes = [
     {path: 'catalogo', component: ProductCatalogComponent},
-    {path: 'producto/:id', component: ViewProductComponent}    
+    //{path: 'producto/:id', component: ViewProductComponent}  esta comentado porque al no estar implementado me daba error en esta branch
+    {path: 'altaProducto', component: ProductCreateComponent},
+    {path: 'listaProducto', component: ProductListComponent},
+    {path: 'ordenesProducto', component: ProductOrdersComponent}
 ];
