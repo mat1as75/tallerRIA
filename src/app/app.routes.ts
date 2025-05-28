6aa60d8a324301a87da4daac0c2fbf6e99ed84a8
smate0 <m4tesilva@gmail.com>
2025-05-28 20:58:06 -0300

    Pagina Principal, Sobre Nostros, NavBar y Footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,13 @@ import { ProductCatalogComponent } from './components/product-catalog/product-ca
 import { ViewProductComponent } from './components/view-product/view-product.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about-us/about-us.component';
 
 export const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'sobre-nosotros', component: AboutComponent },
     {path: 'catalogo', component: ProductCatalogComponent},
     {path: 'producto/:id', component: ViewProductComponent},
     {path: 'login', component: LoginComponent},
