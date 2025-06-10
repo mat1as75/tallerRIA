53e0fcba695f7f1d665f56ba1650d7f815ad53ac
Matias Alfaro Iglesias <138666313+mat1as75@users.noreply.github.com>
2025-06-10 17:24:36 -0300

    Add Payment Confirmation (#14)

---
+++ src/app/components/payment-confirmation/payment-confirmation.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-confirmation',
+  imports: [],
+  templateUrl: './payment-confirmation.component.html',
+  styleUrl: './payment-confirmation.component.scss'
+})
+export class PaymentConfirmationComponent {
+
+  email = 'soporte@tecnostore.com'
+
+}
