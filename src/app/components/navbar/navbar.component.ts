98e59b757c6cf3436a341f287562494c931521c0
santiago <sgonzalr767@gmail.com>
2025-06-12 20:51:55 -0300

    Utilizacion de cookie en NavBar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -8,4 +9,36 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+
+  constructor(private router: Router){}
+
+
+ getCookie(name: string): string | null {
+  const nameEQ = name + "=";
+  const ca = document.cookie.split(';');
+  for (let c of ca) {
+    c = c.trim();
+    if (c.startsWith(nameEQ)) {
+      return decodeURIComponent(c.substring(nameEQ.length));
+    }
+  }
+  return null;
+}
+
+checkCookieRedirect(){
+  const session = this.getCookie('session_ID');
+  console.log('Cookie leída:', session);
+  if (session) {
+    this.router.navigate(['/detallesdecuenta']);
+  } else {
+    this.router.navigate(['/login']);
+  }
+}
+
+}
+
+
+
+
+
