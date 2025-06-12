739127d353dad5fc94e8c9c3c6a4f803d52f9c35
santiago <sgonzalr767@gmail.com>
2025-06-12 13:19:34 -0300

    Error Arreglado

--- src/app/components/detallesdecuenta/detallesdecuenta.component.ts
+++ src/app/components/detallesdecuenta/detallesdecuenta.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-detallesdecuenta',
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './detallesdecuenta.component.html',
   styleUrl: './detallesdecuenta.component.scss'
 })
