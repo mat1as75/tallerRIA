7a498d35f4907686ba003c9cdb3bce0b2bf3fc99
mat1as75 <matialfaro757575@gmail.com>
2025-06-24 16:52:10 -0300

    Update NavBar with ProductSearch

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,14 +1,18 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, inject, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { CookieService } from '../../services/cookies/cookie.service';
 import { CartService } from '../../services/cart/cart.service';
 import { LocalStorageService } from '../../services/local-storage/local-storage.service';
+import { SearchService } from '../../services/search/search.service';
+import { FormsModule } from '@angular/forms';
+import { Category } from '../../interfaces/Category.interface';
+import { ProductService } from '../../services/product/product.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
@@ -16,11 +20,19 @@ export class NavbarComponent implements OnInit{
   private serviceCookie = inject(CookieService)
   private cartService = inject(CartService)
   private localStorageService = inject(LocalStorageService)
+  private searchService = inject(SearchService)
+  private productService = inject(ProductService)
 
   sessionId?: number | undefined
   quantityProductsCart: number = 0
+  searchText = ''
 
-  constructor(private router: Router) {}
+  showCategoryList: boolean = false
+  categories: Category[] = []
+
+  @ViewChild('menuContainer') menuContainerRef!: ElementRef
+
+  constructor(private router: Router, private eRef: ElementRef) {}
 
   ngOnInit(): void {
     const cookieValue = this.serviceCookie.getCookie('session_ID') || localStorage.getItem('session_ID');
@@ -50,4 +62,40 @@ export class NavbarComponent implements OnInit{
     }
   } 
 
+  onSearch() {
+    this.searchService.setSearchTerm(this.searchText)
+    this.router.navigate(['/catalogo'])
+    this.searchText = ''
+  }
+
+  loadCategories() {
+    this.productService.getCategories().subscribe({
+      next: (data) => this.categories = data,
+      error: (err) => console.error('Error cargando categorias ', err)
+    })
+  }
+
+  toggleCategoryMenu() {
+    this.showCategoryList = !this.showCategoryList
+    if (this.showCategoryList && this.categories.length === 0)
+      this.loadCategories()
+  }
+
+  navigateToCategory(categoryId: number | null) {
+    this.searchService.setSearchTerm('')
+
+    if (categoryId === null)
+      this.router.navigate(['/catalogo'], { queryParams: {} })
+    else
+      this.router.navigate(['/catalogo'], { queryParams: { categoryId: categoryId } })
+
+    this.showCategoryList = false
+  }
+
+  @HostListener('document:click', ['$event'])
+  handleClickOutside(event: MouseEvent) {
+    if (!this.menuContainerRef?.nativeElement.contains(event.target)) {
+      this.showCategoryList = false;
+    }
+  }
 }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     apiUrl: 'https://api.escuelajs.co/api/v1',
-    apiphp: 'http://localhost/PHP/tallerPHP/public/index.php'
-    //apiphp: 'http://localhost:8080/PHP/tallerPHP/public/index.php'
+    //apiphp: 'http://localhost/PHP/tallerPHP/public/index.php'
+    apiphp: 'http://localhost:8080/php/tallerPHP/public/index.php'
 };
