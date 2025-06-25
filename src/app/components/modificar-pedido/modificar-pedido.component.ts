e7d6cd8649ce5e12514c658f92983ef489adc22f
mat1as75 <matialfaro757575@gmail.com>
2025-06-25 19:40:45 -0300

    Fix

--- src/app/components/modificar-pedido/modificar-pedido.component.ts
+++ src/app/components/modificar-pedido/modificar-pedido.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { FooterComponent } from '../footer/footer.component';
-import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-modificar-pedido',
-  imports: [FooterComponent,NavbarComponent],
+  imports: [],
   templateUrl: './modificar-pedido.component.html',
   styleUrl: './modificar-pedido.component.scss'
 })
